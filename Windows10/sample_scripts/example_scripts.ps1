84634797a78a76630f453b6b27a17ca4c03ac49a
Ibrahima Diallo <ibrahima.diallo1289@gmail.com>
2023-03-29 21:17:08 -0400

    new script

---
+++ Windows10/sample_scripts/example_scripts.ps1
@@ -0,0 +1,20 @@
+﻿
+# Powershell Version
+#$PSVersionTable
+
+#############
+
+
+# Service status
+#Get-Service | Sort-Object -property Status | Format-Table
+
+#Get-Service | WHERE {$_.Status -eq "Running"} | SELECT displayname 
+
+#Get-ChildItem Env:
+
+#Get-Help Format-Table -Full
+
+#Get-Service -ServiceName 'Dnscache' | Select-Object Name
+
+#Get-Command | Where-Object { $_.parametersets.count -gt 2 } | Format-List Name
+gcm | ? { $_.parametersets.count -gt 2 } | fl Name
